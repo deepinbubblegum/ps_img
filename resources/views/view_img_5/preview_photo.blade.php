2174008118cc68e8d62736ead3b9ae81171221a7
Kittisak Udomthawornkul <kid00345@hotmail.com>
2023-01-24 01:47:39 +0700

    update

--- resources/views/view_img_5/preview_photo.blade.php
+++ resources/views/view_img_5/preview_photo.blade.php
@@ -155,15 +155,20 @@
 
             html2canvas($("#div_pdf")[0]).then(function (canvas) {
                 var imgData = canvas.toDataURL("image/jpeg", 1.0);
-                var pdf = new jsPDF('l', 'pt', [PDF_Width, PDF_Height]);
-                pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width,
-                    canvas_image_height);
-                for (var i = 1; i <= totalPDFPages; i++) {
-                    pdf.addPage(PDF_Width, PDF_Height);
-                    // pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin *
-                    //     4), canvas_image_width, canvas_image_height);
+
+                try {
+                    var pdf = new jsPDF('l', 'pt', [PDF_Width, PDF_Height]);
+                    pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width,
+                        canvas_image_height);
+                    for (var i = 1; i <= totalPDFPages; i++) {
+                        pdf.addPage(PDF_Width, PDF_Height);
+                        // pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin *
+                        //     4), canvas_image_width, canvas_image_height);
+                    }
+                    pdf.save("Image.pdf");
+                } catch (e) {
+                    console.log(e);
                 }
-                pdf.save("Your_PDF_Name.pdf");
             });
         }
 
